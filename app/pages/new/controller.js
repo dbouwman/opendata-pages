559539871776bc264d9df1f8fd35c939def93245
Dave Bouwman <dave@davebouwman.com>
2016-07-28 16:57:44 -0600

    formalized page templates

--- app/pages/new/controller.js
+++ app/pages/new/controller.js
@@ -3,16 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   pagesService: Ember.inject.service('pages-service'),
 
+  msg:'',
+  isWorking:false,
+  
 
   actions: {
     selectTemplate(itemId){
+      this.set('isWorking', true);
       console.log('Selected ' + itemId);
+      this.set('msg', 'Fetching template...');
       this.get('pagesService').getPage(itemId)
       .then((page)=>{
-        page.title = "Page Started from " + page.title + " Template"
+        this.set('msg', 'Cloning template and saving item...');
+        page.title = "Crime " + page.title;
         return this.get('pagesService').create(page);
       })
       .then((result)=>{
+        this.set('msg', 'Loading editor...');
         let newId = result.id;
         this.transitionToRoute('pages.page.edit', newId);
       });
